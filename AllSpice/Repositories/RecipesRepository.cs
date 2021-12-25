41b89eef7a9950ab0947deccc1d3355258575c46
autumnlay <autumn.lay0@gmail.com>
2021-12-25 11:38:32 -0700

    createR

--- AllSpice/Repositories/RecipesRepository.cs
+++ AllSpice/Repositories/RecipesRepository.cs
@@ -18,5 +18,14 @@ namespace AllSpice.Repositories
             string sql = "SELECT * FROM recipes;";
             return _db.Query<Recipe>(sql).ToList();
         }
+        internal Recipe Create(Recipe newRecipe)
+        {
+            string sql = @"INSERT INTO recipes(name, creatorId)
+            VALUES (@Name, @CreatorId);
+            SELECT LAST_INSERT_ID();";
+            int id = _db.ExecuteScalar<int>(sql, newRecipe);
+            newRecipe.Id = id;
+            return newRecipe;
+        }
     }
 }
